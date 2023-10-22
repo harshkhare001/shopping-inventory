348ec08b229a3e934d7352b20bff5263efbf3bdf
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-10-23 00:26:50 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,71 +2,6 @@ var form = document.querySelector("#form");
 
 var list = document.querySelector("#list");
 
-let data = {};
-
-// form.addEventListener("submit", async (e) => {
-//   e.preventDefault();
-//   var itemName = document.getElementById("name");
-//   var itemDesc = document.getElementById("desc");
-//   var itemPrice = document.getElementById("price");
-//   var itemQty = document.getElementById("qty");
-
-//   data = {
-//     name: itemName.value,
-//     desc: itemDesc.value,
-//     price: itemPrice.value,
-//     qty: itemQty.value,
-//   };
-
-//   var li = document.createElement("li");
-//   let id = "";
-//   li.className = "items list-group-item list-group-item-success";
-//   var div1 = document.createElement("div");
-//   var div2 = document.createElement("div");
-//   var span1 = document.createElement("span");
-//   var span2 = document.createElement("span");
-//   var span3 = document.createElement("span");
-//   var span4 = document.createElement("span");
-
-//   span1.textContent = `${data.name} `;
-//   span2.textContent = `${data.desc} `;
-//   span3.textContent = `${data.price} `;
-//   span4.textContent = `${data.qty}`;
-//   div1.appendChild(span1);
-//   div1.appendChild(span2);
-//   div1.appendChild(span3);
-//   div1.appendChild(span4);
-//   li.appendChild(div1);
-
-//   var buy1 = document.createElement("button");
-//   var buy2 = document.createElement("button");
-//   var buy3 = document.createElement("button");
-//   buy1.className = "buy1";
-//   buy1.textContent = "buy1";
-//   buy2.className = "buy2";
-//   buy2.textContent = "buy2";
-//   buy3.className = "buy3";
-//   buy3.textContent = "buy3";
-//   div2.appendChild(buy1);
-//   div2.appendChild(buy2);
-//   div2.appendChild(buy3);
-//   li.appendChild(div2);
-//   list.appendChild(li);
-//   const res = await axios.post("https://crudcrud.com/api/5acd443d6ef54ec3bb6ce76626383c4e/storeData",data);
-//   id = res.data._id;
-//   buy1.id = res.data._id;
-//   buy2.id = res.data._id;
-//   buy3.id = res.data._id;
-//   //   console.log(buy1.id);
-//   //   console.log(buy2.id);
-//   //   console.log(buy3.id);
-
-//   e.target["name"].value = "";
-//   e.target["desc"].value = "";
-//   e.target["price"].value = "";
-//   e.target["qty"].value = "";
-// });
-
 window.addEventListener("DOMContentLoaded", async function renderElements() {
   const users = await axios.get("http://localhost:3000/getProducts");
   users.data.forEach((elem) => {
@@ -78,31 +13,29 @@ window.addEventListener("DOMContentLoaded", async function renderElements() {
     var span2 = document.createElement("span");
     var span3 = document.createElement("span");
     var span4 = document.createElement("span");
-
-    span1.textContent = `${elem.name} `;
-    span2.textContent = `${elem.description} `;
-    span3.textContent = `${elem.price} `;
+    span1.textContent = `${elem.name}  `;
+    span2.textContent = `${elem.description}  `;
+    span3.textContent = `${elem.price}  `;
     span4.textContent = `${elem.Quantity}`;
     div1.appendChild(span1);
     div1.appendChild(span2);
     div1.appendChild(span3);
     div1.appendChild(span4);
     li.appendChild(div1);
-
     var buy1 = document.createElement("button");
     var buy2 = document.createElement("button");
     var buy3 = document.createElement("button");
-    buy1.className = "buy1";
-    buy1.textContent = "buy1";
-    buy2.className = "buy2";
-    buy2.textContent = "buy2";
-    buy3.className = "buy3";
-    buy3.textContent = "buy3";
-    //console.log(elem.id)
+    div1.className = "div1"
+    div2.className = "div2"
+    buy1.className = "buy1 btn-danger";
+    buy1.textContent = "buy 1";
+    buy2.className = "buy2 btn-info";
+    buy2.textContent = "buy 2";
+    buy3.className = "buy3 btn-info";
+    buy3.textContent = "buy 3";
     buy1.id = elem.id;
     buy2.id = elem.id;
     buy3.id = elem.id;
-    //console.log(buy3.id);
     div2.appendChild(buy1);
     div2.appendChild(buy2);
     div2.appendChild(buy3);
@@ -113,10 +46,9 @@ window.addEventListener("DOMContentLoaded", async function renderElements() {
 
 list.addEventListener("click", async (e) => {
   if (e.target.classList.contains("buy1") || e.target.classList.contains("buy2") || e.target.classList.contains("buy3")) {
-    let buyQuantity = Number(e.target.textContent.substring(3));
+    let buyQuantity = Number(e.target.textContent.substring(4));
     
     let id = e.target.id;
-    //console.log(buyQuantity);
     let itemData = e.target.parentElement.parentElement.firstElementChild;
     let itemName = itemData.getElementsByTagName("span")[0].textContent;
     let itemDesc = itemData.getElementsByTagName("span")[1].textContent;
@@ -133,19 +65,18 @@ list.addEventListener("click", async (e) => {
     alert("buy quantity is greater than the available");
     } else if (itemQuantity > 0) {
          var data = {
-         name: itemName,
-        desc: itemDesc,
-        price: itemPrice,
-        qty: itemQuantity - buyQuantity,
-     };
-
-    await axios.put(`http://localhost:3000/updateProduct/${id}`,data);
-    itemData.getElementsByTagName("span")[3].textContent = data.qty;
+          name: itemName,
+          desc: itemDesc,
+          price: itemPrice,
+          qty: itemQuantity - buyQuantity,
+        }; 
+      await axios.post(`http://localhost:3000/updateProduct/${id}`,data);
+      itemData.getElementsByTagName("span")[3].textContent = data.qty;
     }
     console.log(data);
     if (itemQuantity == 0) {
         alert("Out Of Stock");
+        await axios.get(`http://localhost:3000/deleteProduct/${id}`);
     }
   }
-
 });
