f3a7561ed81bac5aa1930a504448a9c45ff6720c
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-10-22 15:57:06 +0530

    initial commit

---
+++ controllers/product.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const Product = require('../models/product');
+
+exports.getHomePage = (req,res,next)=>{
+    res.sendFile(path.join(__dirname, "../", "public", "views", "index.html"))
+}
+
+exports.addProduct = (req,res,next)=>{
+    const name = req.body.name;
+    const description = req.body.desc;
+    const price = req.body.price;
+    const quantity = req.body.qty;
+
+    Product.create({
+        name:name,
+        description:description,
+        price:price,
+        Quantity:quantity
+    })
+    .then(result=>{
+        console.log('Product added');
+        res.redirect('/');
+    })
+    .catch((err)=>console.log(err));
+}
+
+exports.getProducts = (req,res,next)=>{
+    Product.findAll()
+    .then(products=>{
+        res.json(products);
+    })
+    .catch(err=>console.log(err));
+}
+
+exports.updateProduct = (req,res,next)=>{
+    const productId = req.params.id;
+    console.log(productId);
+    console.log(req.body);
+    const name = req.body.name;
+    const description = req.body.desc;
+    const price = req.body.price;
+    const quantity = req.body.qty;
+    Product.update({
+        name: name,
+        description : description,
+        price : price,
+        Quantity : quantity
+    },
+        {where : 
+            {
+                id:productId
+            }
+        }
+    )
+    .then((result)=>{
+        res.redirect('/');
+    })
+    .catch(err=>console.log(err));
+}
